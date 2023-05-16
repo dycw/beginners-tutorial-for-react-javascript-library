66ad723f81680807dfb9adb22ff1fc04be58d6f0
Derek Wan <d.wan@icloud.com>
2023-05-16 13:23:17 +0800

    Working through intro

---
+++ intro/index.jsx
@@ -0,0 +1,14 @@
+function MainContent() {
+  return <div>Main content</div>;
+}
+ReactDOM.render(
+  <nav>
+    <h1>Website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>,
+  document.getElementById("root"),
+);
